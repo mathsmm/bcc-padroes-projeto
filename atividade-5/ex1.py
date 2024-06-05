a472b9841b5f6af9fef4783f2ed8d4e55f03764f
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2024-06-04 22:29:52 -0300

    Atividade 5 terminada

--- atividade-5/ex1.py
+++ atividade-5/ex1.py
@@ -23,20 +23,25 @@ class State(ABC):
 class MaquinaBolinhas:
     def __init__(self, qtd_bolinhas: int) -> None:
         self.estoque = qtd_bolinhas
-        self.est_sem_credito = SemCredito(self)
-        self.est_com_credito = ComCredito(self)
-        self.est_esgotado    = Esgotado(self)
-        self.est_vendido     = Vendido(self)
-        self.est_vencedor    = Vencedor(self)
-        self.estado_atual    = self.est_sem_credito
+        self.est_sem_credito: State = SemCredito(self)
+        self.est_com_credito: State = ComCredito(self)
+        self.est_esgotado: State    = Esgotado(self)
+        self.est_vendido: State     = Vendido(self)
+        self.est_vencedor: State    = Vencedor(self)
+        self.estado_atual: State    = self.est_sem_credito
 
 # CONCRETAS
-class SemCredito:
+class SemCredito(State):
     def __init__(self, maquina: MaquinaBolinhas) -> None:
         self.maq: MaquinaBolinhas = maquina
 
     def inserir_moeda(self):
-        self.maq.estado_atual = self.maq.est_com_credito
+        if self.maq.estoque <= 0:
+            self.maq.estado_atual = self.maq.est_esgotado
+            print("Máquina sem bolinhas")
+        else:
+            self.maq.estado_atual = self.maq.est_com_credito
+            print("Moeda inserida")
 
     def ejetar_moeda(self):
         print("Você não inseriu moeda")
@@ -47,7 +52,7 @@ class SemCredito:
     def entregar(self):
         print("Você acionou a alavanca mas não inseriu moeda")
 
-class ComCredito:
+class ComCredito(State):
     def __init__(self, maquina: MaquinaBolinhas) -> None:
         self.maq: MaquinaBolinhas = maquina
 
@@ -62,13 +67,15 @@ class ComCredito:
         i = random.randint(1, 10)
         if i == 1:
             self.maq.estado_atual = self.maq.est_vencedor
+            print("Parabéns! Você ganhou uma bolinha extra")
         else:
             self.maq.estado_atual = self.maq.est_vendido
+            print("Uma bolinha te espera")
 
     def entregar(self):
         print("Nenhuma bolinha foi entregue")
 
-class Vendido:
+class Vendido(State):
     def __init__(self, maquina: MaquinaBolinhas) -> None:
         self.maq: MaquinaBolinhas = maquina
 
@@ -90,7 +97,7 @@ class Vendido:
         else:
             self.maq.estado_atual = self.maq.est_sem_credito
 
-class Esgotado:
+class Esgotado(State):
     def __init__(self, maquina: MaquinaBolinhas) -> None:
         self.maq: MaquinaBolinhas = maquina
 
@@ -106,7 +113,7 @@ class Esgotado:
     def entregar(self):
         print("Nenhuma bolinha foi fornecida")
 
-class Vencedor:
+class Vencedor(State):
     def __init__(self, maquina: MaquinaBolinhas) -> None:
         self.maq: MaquinaBolinhas = maquina
 
@@ -126,13 +133,23 @@ class Vencedor:
             print("Você teve sorte! Foram fornecidas duas bolinhas")
         else:
             self.maq.estoque -= 1
-            print("Bolinha fornecida")
+            print("Uma bolinha fornecida")
 
-        if self.maq.estoque <= 0:
-            self.maq.estado_atual = self.maq.est_esgotado
-        else:
-            self.maq.estado_atual = self.maq.est_sem_credito
+        self.maq.estado_atual = self.maq.est_sem_credito
 
-maq = MaquinaBolinhas()
+print("Criando a máquina com 10 bolinhas")
+maq = MaquinaBolinhas(10)
+print("====================")
 
-print("Inserindo moeda")
+maq.estado_atual.inserir_moeda()
+maq.estado_atual.virar_manivela()
+maq.estado_atual.entregar()
+print("====================")
+
+maq.estado_atual.inserir_moeda()
+maq.estado_atual.ejetar_moeda()
+maq.estado_atual.virar_manivela()
+maq.estado_atual.inserir_moeda()
+maq.estado_atual.entregar()
+maq.estado_atual.virar_manivela()
+maq.estado_atual.entregar()
